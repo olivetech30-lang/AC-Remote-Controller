c307efdbced44c511cbc64d3bc6435219ed97cf0
Olive tech <olivetech30@gmail.com>
2025-12-25 20:41:00 +0500

    Delete AC controller directory

--- AC controller/vercel/api/ac/ack.js
+++
@@ -1,7 +0,0 @@
-// api/ac/ack.js
-export default (req, res) => {
-  if (global.acState) {
-    global.acState.executed = true;
-  }
-  res.json({ status: "acknowledged" });
-};
--- AC controller/vercel/api/ac/command.js
+++
@@ -1,5 +0,0 @@
-// api/ac/command.js
-export default (req, res) => {
-  const state = global.acState || { command: "none", executed: true };
-  res.json(state);
-};
